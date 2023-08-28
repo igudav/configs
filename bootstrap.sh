f7f296ad755cb4ed2a334007a6af68134afa9678
Igor Udovichenko <igudav@ya.ru>
2023-08-28 22:35:21 +0700

    bootstrap, needs testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,6 +28,8 @@ ln -rs configs/.gitignore
 
 # install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/c92adb3c479d70bb29f4399a808c972ef41510e7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 # configuring vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
