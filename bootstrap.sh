8ae1db2b23505d4b4130bc3829acfd51b5b0c496
Igor Udovichenko <igudav@ya.ru>
2023-08-28 22:27:44 +0700

    bootstrap fixed

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,12 +21,14 @@ if [ -d .vim ]; then
     rm -rf .vim
 fi
 mkdir .vim
-ln -rs configs/.vim/ftplugin .vim/ftplugin
 ln -rs configs/.vim/coc-settings.json .vim/coc-settings.json
 
 ln -rs configs/.tmux.conf
 ln -rs configs/.gitignore
 
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/c92adb3c479d70bb29f4399a808c972ef41510e7/install.sh | bash
+
 # configuring vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PluginInstall +qall
